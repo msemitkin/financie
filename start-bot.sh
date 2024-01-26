db0a5ec73d560248e9da9c275d1eb620294ad828
Mykhailo Semitkin <michael.semitkin@gmail.com>
2024-01-27 00:35:19 +0200

    move script to a separate file

---
+++ start-bot.sh
@@ -0,0 +1,10 @@
+cd /root/financie
+# Send SIGTERM to the existing instance
+pkill -f FinancieBot
+# Wait for the process to fully terminate
+while pgrep -f FinancieBot > /dev/null; do
+echo "Waiting for FinancieBot to terminate..."
+sleep 1
+done
+echo "FinancieBot terminated. Starting new instance."
+exec -a FinancieBot java -jar financie-1.3.1.jar &
