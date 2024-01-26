025b16777e179ee99ac21ced6e4db9401008fa68
Mykhailo Semitkin <michael.semitkin@gmail.com>
2024-01-26 22:38:14 +0200

    disabled context test

--- src/test/java/com/github/msemitkin/financie/FinancieApplicationTests.java
+++ src/test/java/com/github/msemitkin/financie/FinancieApplicationTests.java
@@ -1,10 +1,12 @@
 package com.github.msemitkin.financie;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = FinancieApplication.class)
+@Disabled
 class FinancieApplicationTests {
 
     @Test
