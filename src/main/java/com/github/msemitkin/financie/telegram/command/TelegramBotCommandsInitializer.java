ed92e04e54ebdfb63a64c153f2369f08a3598f67
Mykhailo Semitkin <michael.semitkin@gmail.com>
2024-02-05 22:43:05 +0200

    added translations for /commands

--- src/main/java/com/github/msemitkin/financie/telegram/command/TelegramBotCommandsInitializer.java
+++ src/main/java/com/github/msemitkin/financie/telegram/command/TelegramBotCommandsInitializer.java
@@ -1,25 +1,23 @@
 package com.github.msemitkin.financie.telegram.command;
 
+import com.github.msemitkin.financie.locale.LanguageCode;
+import com.github.msemitkin.financie.resources.ResourceService;
 import com.github.msemitkin.financie.telegram.api.TelegramApi;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
-import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 @Component
 public class TelegramBotCommandsInitializer implements InitializingBean {
     private final TelegramApi telegramApi;
-    private final List<BotCommand> commands;
 
-    public TelegramBotCommandsInitializer(
-        TelegramApi telegramApi,
-        List<BotCommand> commands
-    ) {
+    public TelegramBotCommandsInitializer(TelegramApi telegramApi) {
         this.telegramApi = telegramApi;
-        this.commands = commands;
     }
 
     @Override
@@ -28,8 +26,33 @@ public class TelegramBotCommandsInitializer implements InitializingBean {
     }
 
     private void initCommands() {
-        telegramApi.execute(SetMyCommands.builder()
-            .commands(commands).scope(BotCommandScopeDefault.builder().build())
-            .build());
+        List<Locale> locales = Arrays
+            .stream(LanguageCode.values())
+            .map(LanguageCode::getCode)
+            .map(Locale::of)
+            .toList();
+        var botCommands = Arrays
+            .stream(BotCommand.values())
+            .toList();
+        for (Locale locale : locales) {
+            var commandsForLocale = getCommandsForLocale(locale, botCommands);
+            telegramApi.execute(SetMyCommands.builder()
+                .languageCode(locale.getLanguage())
+                .commands(commandsForLocale)
+                .scope(BotCommandScopeDefault.builder().build())
+                .build());
+        }
+    }
+
+    private List<org.telegram.telegrambots.meta.api.objects.commands.BotCommand> getCommandsForLocale(
+        Locale locale,
+        List<BotCommand> botCommands
+    ) {
+        return botCommands.stream()
+            .map(command -> org.telegram.telegrambots.meta.api.objects.commands.BotCommand.builder()
+                .command(command.getCommand())
+                .description(ResourceService.getValue(command.getDescriptionCode(), locale))
+                .build())
+            .toList();
     }
 }
