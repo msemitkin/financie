55f79924fd54106bb5712c69d354cab12f13ea3a
Mykhailo Semitkin <michael.semitkin@gmail.com>
2024-04-07 14:14:56 +0300

    enable token filter

--- src/main/java/com/github/msemitkin/financie/config/TokenAuthFilter.java
+++ src/main/java/com/github/msemitkin/financie/config/TokenAuthFilter.java
@@ -6,11 +6,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-//@Component
+@Component
 public class TokenAuthFilter extends OncePerRequestFilter {
     private final String token;
 
