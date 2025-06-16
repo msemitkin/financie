e21279c5549bc53af3c327cb722b1fe11b23b005
Mykhailo Semitkin <michael.semitkin@gmail.com>
2025-06-16 19:24:44 +0300

    improve test to handle daylight saving time

--- src/test/java/com/github/msemitkin/financie/timezone/TimeZoneUtilsTest.java
+++ src/test/java/com/github/msemitkin/financie/timezone/TimeZoneUtilsTest.java
@@ -1,25 +1,34 @@
 package com.github.msemitkin.financie.timezone;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalDateTime;
 import java.time.YearMonth;
 import java.time.ZoneId;
-import java.time.ZoneOffset;
+import java.util.stream.Stream;
 
 class TimeZoneUtilsTest {
 
-    @Test
-    void startOfMonthForZoneIdIsConvertedToUtc() {
-        ZoneId zoneId = ZoneId.of("Europe/Berlin");
-        YearMonth yearMonth = YearMonth.of(2024, 2);
+    static Stream<Arguments> monthAndExpectedUtc() {
+        return Stream.of(
+            org.junit.jupiter.params.provider.Arguments.of(
+                YearMonth.of(2024, 2), // Before DST
+                LocalDateTime.of(2024, 1, 31, 23, 0)
+            ),
+            org.junit.jupiter.params.provider.Arguments.of(
+                YearMonth.of(2024, 7), // During DST
+                LocalDateTime.of(2024, 6, 30, 22, 0)
+            )
+        );
+    }
 
-        LocalDateTime expected = yearMonth
-            .atDay(1)
-            .atStartOfDay(zoneId)
-            .withZoneSameInstant(ZoneOffset.UTC)
-            .toLocalDateTime();
+    @ParameterizedTest
+    @MethodSource("monthAndExpectedUtc")
+    void getUTCStartOfTheMonthInTimeZone_shouldBeConvertedToUtc_regardlessOfDst(YearMonth yearMonth, LocalDateTime expected) {
+        ZoneId zoneId = ZoneId.of("Europe/Berlin");
 
         LocalDateTime actual = TimeZoneUtils.getUTCStartOfTheMonthInTimeZone(yearMonth, zoneId);
 
