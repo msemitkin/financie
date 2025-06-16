d4c15cff62538ca565507fa6bcdc2c5881d1aa76
Mykhailo Semitkin <michael.semitkin@gmail.com>
2025-06-16 19:24:44 +0300

    improve test to handle daylight saving time

--- src/main/java/com/github/msemitkin/financie/timezone/TimeZoneUtils.java
+++ src/main/java/com/github/msemitkin/financie/timezone/TimeZoneUtils.java
@@ -2,11 +2,9 @@ package com.github.msemitkin.financie.timezone;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
 import java.time.YearMonth;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
-import java.time.temporal.TemporalAdjusters;
 
 public class TimeZoneUtils {
     private TimeZoneUtils() {
@@ -24,13 +22,7 @@ public class TimeZoneUtils {
     }
 
     public static LocalDateTime getUTCStartOfTheMonthInTimeZone(ZoneId zoneId) {
-        OffsetDateTime startOfTheMonth = OffsetDateTime.now(zoneId)
-            .with(TemporalAdjusters.firstDayOfMonth())
-            .withHour(0)
-            .withMinute(0)
-            .withSecond(0)
-            .withNano(0);
-        return LocalDateTime.ofInstant(startOfTheMonth.toInstant(), ZoneId.of("UTC"));
+        return getUTCStartOfTheMonthInTimeZone(YearMonth.now(zoneId), zoneId);
     }
 
     public static LocalDateTime getUTCStartOfTheMonthInTimeZone(YearMonth yearMonth, ZoneId zoneId) {
